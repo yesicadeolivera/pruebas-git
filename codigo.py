8c7cb13daf9621a2a52d2c477ef84d4ea63fe278
yesica de olivera <yesika.99.lujan@gmail.com>
2018-04-21 16:16:48 -0300

    Se han agregado 2 lineas en codigo.py

--- codigo.py
+++ codigo.py
@@ -4,5 +4,6 @@ b=int(input("Ingrese segundo numero: "))
 suma=a+b
 resta=a-b
 division=a/b
+multi=a*b
 print "Resultados: "
-print "Suma={s},  Resta={r}, Division={d}".format(s=suma,r=resta, d=division)
+print "Suma={s},  Resta={r}, Division={d}, Multiplicacion={m}".format(s=suma,r=resta, d=division,m=multi)
